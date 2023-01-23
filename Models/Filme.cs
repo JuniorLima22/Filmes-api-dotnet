12c3a0720fbb0b67ecb1450274acfc821f3d51bb
Junior Lima <juniorlima.dev@gmail.com>
2023-01-23 10:34:05 -0300

    feat: Adicionando validação dos dados ao cadastrar filme

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O titulo do filme é obigatório")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero nao pode exceder 50 caracteres")]
     public string Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obigatório")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600")]
     public int Duracao { get; set; }
 }
